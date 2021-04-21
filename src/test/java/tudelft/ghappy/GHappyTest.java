e6cb351e2c582dd82bca2779f0da9cc76b61ba06
Marcél Busschers <m.busschers@student.vu.nl>
2021-04-21 12:54:19 +0200

    Added tests for GHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,19 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name = "{0} -> {1}")
+    @CsvSource({
+            "xxggxx, true",
+            "xxgxx, false",
+            "xxggyygxx, false"
+    })
+    public void testAlgorithm(String word, boolean outcome) {
+        boolean result = new GHappy().gHappy(word);
+        Assertions.assertEquals(outcome, result);
+    }
 }
